816708b6c82b21487307f3a10d3ae3fbb2c36807
Jackson Medeiros <8601315+1schwarzbier@users.noreply.github.com>
2022-01-28 00:26:38 -0300

    Update api.js
     - Now using promises to POST request to generate a new layout
     - Removed unnecessary files (test.html)

--- js/api.js
+++ js/api.js
@@ -1,30 +1,25 @@
 function sendLayout()
 {
-    console.log("CHEGUEI AQUI")
+    const ajaxUrl = "http://127.0.0.1:8000/junk";
 
-    console.log("criando form data")
-    var formData = new FormData();
-    formData.append("layout", getLayoutObject())
+    let payload = async function(){
+        return await getLayoutObject()
+    }
 
-    console.log("meu form-data")
-    console.log(formData)
-                
-    var ajaxUrl = "https://bacardiweb.herokuapp.com/layout";
-    
-    $.ajax({
-        url : ajaxUrl,
-        type : "POST",
-        data : formData,
-        // both 'contentType' and 'processData' parameters are
-        // required so that all data are correctly transferred
-        contentType : false,
-        processData : false
-    }).done(function(response){
-        console.log(response.status)
-    }).fail(function(){
-        // Here you should treat the http errors (e.g., 403, 404)
-        console.log(response.status)
-    }).always(function(){
-        alert("AJAX request FUNCIONOU NUNCA DUVIDEI!");
+    payload().then(function(data){
+        console.log(data)
+        $.ajax({
+            url : ajaxUrl,
+            type : "POST",
+            data : data,
+            processData: false,
+            contentType: 'application/json',
+        }).done(function(response){
+            console.log(response.status)
+        }).fail(function(response){
+            console.log(response.status)
+        }).always(function(){
+            alert("AJAX request FUNCIONOU NUNCA DUVIDEI!");
+        });
     });
 }
--- js/utils.js
+++ js/utils.js
@@ -1,44 +1,118 @@
-function getLayoutObject(){
+async function getLayoutObject() {
     const obj = new Object();
     const grid = new Object();
     const size = new Object();
     obj.size = getSize();
     obj.grid = getGrid();
-    obj.layout = getLayout();
+    obj.layout = await getLayout();
+    console.log(obj.layout)
     return JSON.stringify(obj);
 }
 
-function getSize(){
+function getSize() {
     const size = new Object();
-    size.width = document.getElementsByName('cardWidth')[0].value;
-    size.height = document.getElementsByName('cardHeight')[0].value;
-    size.unit = document.getElementsByName('cardUnit')[0].value;
+    // size.width = document.getElementsByName('cardWidth')[0].value;
+    // size.height = document.getElementsByName('cardHeight')[0].value;
+    // size.unit = document.getElementsByName('cardUnit')[0].value;
+    size.width = 3;
+    size.height = 5;
+    size.unit = "mm";
     return size;
 }
 
-function getGrid(){
+function getGrid() {
     const grid = new Object();
-    grid.width = document.getElementsByName('gridWidth')[0].value;
-    grid.height = document.getElementsByName('gridHeight')[0].value;
+    // grid.width = document.getElementsByName('gridWidth')[0].value;
+    // grid.height = document.getElementsByName('gridHeight')[0].value;
+    grid.width = 5
+    grid.height = 1
     return grid;
 }
 
-function getLayout(){
+function readFileAsync(file) {
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+
+        reader.onload = () => {
+            resolve(reader.result);
+        };
+
+        reader.onerror = reject;
+
+        reader.readAsArrayBuffer(file);
+    })
+}
+
+function base64ArrayBuffer(arrayBuffer) {
+    var base64 = ''
+    var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+    var bytes = new Uint8Array(arrayBuffer)
+    var byteLength = bytes.byteLength
+    var byteRemainder = byteLength % 3
+    var mainLength = byteLength - byteRemainder
+
+    var a, b, c, d
+    var chunk
+
+    for (var i = 0; i < mainLength; i = i + 3) {
+
+        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+
+        a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
+        b = (chunk & 258048) >> 12 // 258048   = (2^6 - 1) << 12
+        c = (chunk & 4032) >> 6 // 4032     = (2^6 - 1) << 6
+        d = chunk & 63               // 63       = 2^6 - 1
+
+        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+    }
+
+    if (byteRemainder == 1) {
+        chunk = bytes[mainLength]
+
+        a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+
+        b = (chunk & 3) << 4 // 3   = 2^2 - 1
+
+        base64 += encodings[a] + encodings[b] + '=='
+    } else if (byteRemainder == 2) {
+        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+
+        a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
+        b = (chunk & 1008) >> 4 // 1008  = (2^6 - 1) << 4
+
+        c = (chunk & 15) << 2 // 15    = 2^4 - 1
+
+        base64 += encodings[a] + encodings[b] + encodings[c] + '='
+    }
+
+    return base64
+}
+
+async function getLayout() {
     const array = [];
     const layouts = Array.from(document.getElementsByClassName('layoutForm'));
-    for(i of layouts){
+    for (i of layouts) {
         let campos = Array.from(i.getElementsByClassName('layoutField'))
         obj = new Object();
         obj[campos[0].name] = campos[0].value
         obj[campos[1].name] = campos[1].value
-        obj[campos[2].name] = campos[2].value
-        obj[campos[3].name] = campos[3].value
-        obj[campos[4].name] = campos[4].value
-        if(obj.type == 'image'){
-            obj[campos[6].name] = campos[6].value
+        obj[campos[2].name] = parseInt(campos[2].value)
+        obj[campos[3].name] = parseInt(campos[3].value)
+        obj[campos[4].name] = parseInt(campos[4].value)
+        if (obj.type == 'image') {
+            try {
+                // TODO: Trocar pelo elemento imagem que esta sendo iterado
+                let file = document.querySelector('input[type=file]').files[0];
+                let contentBuffer = await readFileAsync(file);
+                obj[campos[6].name] = base64ArrayBuffer(contentBuffer)
+            }
+            catch (err) {
+                console.log(err);
+            }
         }
-        else{
-            obj[campos[5].name] = campos[5].value
+        else {
+            obj[campos[5].name] = parseInt(campos[5].value)
         }
         array.push(obj)
     }
